c97677cff07babb4b8b186013fc75bd330884db0
Andre <eusoubrasileiro@gmail.com>
2018-07-12 15:01:33 -0300

    Backtesting engine 95% ready. A little tweek with days and I can use. Need to run Logistic Regression / NN with lower accuracy for prediction.

--- Tools/Backtesting.py
+++ Tools/Backtesting.py
@@ -22,3 +22,24 @@ def Nstocks(enterprice, exgain, minp=300, costorder=15., ir=0.2):
     This guarantees a `MinP` per order"""
     # round stocks to 100's
     return np.ceil(int((minp+costorder*2)/((1-ir)*enterprice*exgain))/100)*100
+    
+def MoneyBack(enter_price, close_price, 
+            quantity, direction, ir=0.2, cost_order=12):
+    """
+    Money back after an order is closed day trade
+    direction=1/-1 buy/sell == long/short
+
+    Costs:
+    20% taxes day trade
+    12 R$ cost per order (may be improved!!)
+
+    Just for stocks yet!
+    """
+    # delta money (profit or loss) (positive or negative)
+    # already deduce cost of order
+    dm = ((close_price-enter_price)*
+    quantity*direction-cost_order)
+    if dm > 0: # deduce taxes day trade 20%
+        dm *= (1-ir)
+    return enter_price*quantity + dm
+

190cf47e0195e4d5939c2e2fa9cb23b3c3fd81d6
eusoubrasileiro <1t@ng1rg1gh3nd3l>
2018-09-13 23:52:12 -0300

    Some small fixes.

--- Tools/prepareData.py
+++ Tools/prepareData.py
@@ -9,7 +9,7 @@ from sklearn.utils import shuffle
 from sklearn.preprocessing import StandardScaler
 import talib as ta
 
-def createTargetVector(X, targetsymbol, view=True):
+def createTargetVector(X, targetsymbol, span=120, view=True):
     """
     Create y target vector (column) shift back in time 120 minutes.
 
@@ -17,11 +17,10 @@ def createTargetVector(X, targetsymbol, view=True):
         when training we can drop the last samples that are nans
         default behavior
     """
-    span=120 # best long trend guide until now
     X['ema'] = ta.EMA(X[targetsymbol].values, span)
     X.loc[ X[targetsymbol] > X.ema, 'y'] = 1
     X.loc[ X[targetsymbol] < X.ema, 'y'] = 0
-    X.y = X.y.shift(-span) # has to be 120!!!
+    X.y = X.y.shift(-span)
     if view:
         f, axr = plt.subplots(2, sharex=True, figsize=(15,4))
         f.subplots_adjust(hspace=0)
@@ -51,7 +50,7 @@ def ScaleNormalize(X, stats):
     if stats is None: # calculate mean and variance for each colum
     # and normalize than for mean 0 and variance 1
         for col in X: # make variance 1 and mean 0
-            X[col] = (X[col]-np.mean(X[col]))/np.sdt(X[col])
+            X[col] = (X[col]-np.mean(X[col]))/np.std(X[col])
     else: # mean and variance were specied
         for col in X: # make variance 1 and mean 0
             X[col] = (X[col]-stats.loc[col, 'mean'])/stats.loc[col, 'std']
@@ -86,23 +85,23 @@ def createCrossedFeatures(df, span=60):
         df['rsi_3'+quote] = ta.RSI(df[quote].values, span*3)
     return df
 
-def getSimilarFeatures(X, correlation=95.):
+def getSimilarFeatures(X, correlation=0.95):
     """
     calculate features with more than 95% absolute correlation
     note : very slow for calculate on big data frames
     return: column names
     might be used to remove (drop) highly correlated columns
     """
-    correlation = X.corr()
-    corr_matrix = correlation.abs()
+    xcorrelation = X.corr()
+    corr_matrix = xcorrelation.abs()
     # Select upper triangle of correlation matrix
     upper = corr_matrix.where(np.triu(np.ones(corr_matrix.shape), k=1).astype(np.bool))
     # Find index of feature columns with correlation greater than 0.95
-    similar_columns = [column for column in upper.columns if any(upper[column] > 0.95)]
+    similar_columns = [column for column in upper.columns if any(upper[column] > correlation)]
     return similar_columns
 
 def GetTrainingPredictionVectors(X, selected=None, stats=None, targetsymbol='PETR4_C',
-        verbose=True):
+        verbose=True, correlation=0.98, span=120):
     """
     Calculate features for NN training and target binary class.
     Returns X, y, Xp (--future prediction--)
@@ -119,12 +118,12 @@ def GetTrainingPredictionVectors(X, selected=None, stats=None, targetsymbol='PET
         statistical mean and variance for each collumn in X DataFrame
         for normalization
     """
-    X, y, indexp = createTargetVector(X, targetsymbol=targetsymbol, view=verbose)
+    X, y, indexp = createTargetVector(X, targetsymbol=targetsymbol, view=verbose, span=span)
     LogVols(X)
-    X = createCrossedFeatures(X)
+    X = createCrossedFeatures(X, span=span)
 
     if selected is None:  # calculate correlations and remove by cut-off
-        X = RemoveSimilarFeatures(X, correlation=correlated)
+        X = X[getSimilarFeatures(X, correlation)]
     else: # select only desired columns previouly backtested
         X = X[selected]
 
@@ -138,10 +137,3 @@ def GetTrainingPredictionVectors(X, selected=None, stats=None, targetsymbol='PET
     X.drop(Xp.index, inplace=True)
     y = y[X.index] ## update training vector, this already removes the last 120 nan values
     return X, y, Xp
-
-def GetTrainingVectors(X, targetsymbol='PETR4_C', verbose=True):
-    """
-    calls GetTrainingPredictionVectors
-    """
-    X, y, Xp = GetTrainingPredictionVectors(X, targetsymbol, verbose=verbose)
-    return X, y

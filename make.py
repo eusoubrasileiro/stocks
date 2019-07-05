44a5890dc44dd7cad8266f1d4c87a2ab81c6b4bb
eusoubrasileiro <1t@ng1rg1gh3nd3l>
2019-07-04 21:26:37 -0300

    Python runnable make custom commands.

--- make.py
+++ make.py
@@ -1,4 +1,4 @@
-
+#!/usr/bin/env python
 import subprocess
 from argparse import ArgumentParser
 
@@ -6,28 +6,29 @@ mt5advpath = r"/home/andre/.wine/drive_c/Program\ Files/Rico\ MetaTrader\ 5/MQL5
 mt5cmpath = r"/home/andre/.wine/drive_c/users/andre/Application\ Data/MetaQuotes/Terminal/Common/Files/"
 
 parser = ArgumentParser()
-parser.add_argument("-meta5", dest="meta5", default=False, action="store_false",
+parser.add_argument("-meta5", dest="meta5", default=False, action="store_true",
                     help="link all *.mql and *.mqh files to MetaTrader 5 Advisors Path")
-parser.add_argument("-clean", dest="clean", default=False, action="store_false",
+parser.add_argument("-clean", dest="clean", default=False, action="store_true",
                     help="clean all symlinks")
-parser.add_argument("-newdata", dest="newdata", default=False, action="store_false",
+parser.add_argument("-newdata", dest="newdata", default=False, action="store_true",
                     help="copy new Metatrader 5 binary files to data folder")
 
 args = parser.parse_args()
 
 if args.clean:
-    subprocess.call("cd /home/andre/.wine/drive_c/Program Files/MetaTrader 5/MQL5/Experts/Advisors")
-    subprocess.call("find . -type l -exec unlink \{\} \;")
+    subprocess.call("""cd '/home/andre/.wine/drive_c/Program Files/MetaTrader 5/MQL5/Experts/Advisors';
+                        find . -type l -exec unlink \{\} \;""", shell=True)
 
 if args.meta5:
-    subprocess.call("cd /home/andre/.wine/drive_c/Program Files/MetaTrader 5/MQL5/Experts/Advisors")
-    subprocess.call("find . -type l -exec unlink \{\} \;")
+    subprocess.call("""cd '/home/andre/.wine/drive_c/Program Files/MetaTrader 5/MQL5/Experts/Advisors';
+                        find . -type l -exec unlink \{\} \;""", shell=True)
     # make symlinks from stocks folder to Metatrader folder
-    subprocess.call("cd /home/andre/.wine/drive_c/Program Files/MetaTrader 5/MQL5/Experts/Advisors")
-    subprocess.call("ln -s ~/Projects/stocks/mt5/*.mq* .")
+    subprocess.call("""cd '/home/andre/.wine/drive_c/Program Files/MetaTrader 5/MQL5/Experts/Advisors';
+                ln -s /home/andre/Projects/stocks/mt5/*.mq* .""", shell=True)
 
 if args.newdata:
-    subprocess.call("cp /home/andre/.wine/drive_c/Program\ Files/MetaTrader\ 5/MQL5/Files/* ~/Projects/stocks/data/")
+    subprocess.call("""cd '/home/andre/.wine/drive_c/Program Files/MetaTrader 5/MQL5/Files';
+                    cp * /home/andre/Projects/stocks/data """, shell=True)
 
 # daemon:
 # 	python -m algos.mt5daemons.buybandsd.py

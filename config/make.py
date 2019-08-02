0ea5c7153ee7d6dde6959db2706154be43630f1c
André <eusoubrasileiro@gmail.com>
2019-08-01 21:40:39 -0300

    config make generic for work or home

--- config/make.py
+++ config/make.py
@@ -2,6 +2,7 @@
 import subprocess
 import os, sys, glob
 import shutil
+import platform
 from argparse import ArgumentParser
 
 mt5advpath = r"/home/andre/.wine/drive_c/Program\ Files/Rico\ MetaTrader\ 5/MQL5/Experts/Advisors/"
@@ -22,15 +23,26 @@ parser.add_argument("-cpdll", dest="cpdll", default=False, action="store_true",
 args = parser.parse_args()
 
 if os.name == 'nt':
-    # repository and libraries paths
-    repopath = r'D:\Users\andre.ferreira\Projects\stocks'
-    armadilloroot  = r"D:\Users\andre.ferreira\Projects\armadillo-code-9.600.x" # c++ library
-    # local user paths for MetaTrader 5 installation
-    usermt5hash = '8B052D0699A0083067EBF3A36123603B' # represents the local MetaTrader 5 installation
-    usermt5path = r"D:\Users\andre.ferreira\AppData\Roaming\MetaQuotes"
-    if args.cppbuild:
+    # getting which machine we are
+    if platform.uname()[5] == 'Intel64 Family 6 Model 158 Stepping 9, GenuineIntel': # home
+        # repository and libraries paths
+        repopath = r"C:\Users\andre\Projects\stocks"
+        armadilloroot  = r"C:\Users\andre\Projects\armadillo-code-9.600.x" # c++ library
+        # local user paths for MetaTrader 5 installation
+        usermt5hash = "8B052D0699A0083067EBF3A36123603B" # represents the local MetaTrader 5 installation
+        usermt5path = r"C:\Users\andre\AppData\Roaming\MetaQuotes"
+        # command to create build env using vsbuildtools default installation paths
+        vsbuildenvcmd = r"C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat amd64"
+    else: # work
+        # repository and libraries paths
+        repopath = r'D:\Users\andre.ferreira\Projects\stocks'
+        armadilloroot  = r"D:\Users\andre.ferreira\Projects\armadillo-code-9.600.x" # c++ library
+        # local user paths for MetaTrader 5 installation
+        usermt5hash = '8B052D0699A0083067EBF3A36123603B' # represents the local MetaTrader 5 installation
+        usermt5path = r"D:\Users\andre.ferreira\AppData\Roaming\MetaQuotes"
         ### Using vs build tools
-        vsenvsetup = os.path.join(repopath, r'config\vsbuildtools.bat')
+        vsbuildenvcmd = os.path.join(repopath, r'config\vsbuildtools.bat')
+    if args.cppbuild:
         armadilloincludes = os.path.join(armadilloroot, "include")
         armadillolibblas64 = os.path.join(armadilloroot, r"examples\lib_win64\blas_win64_MT.lib")
         cpppath = os.path.join(repopath, r"mt5\cpp")
@@ -44,7 +56,7 @@ if os.name == 'nt':
         ## /D make a predefinition of symbol BUILDING_DLL
         ## Windows & is the equivalent of ; on linux
         compile = ("cd "+ cpppath + " & " +
-                    vsenvsetup +" & "+
+                    vsbuildenvcmd +" & "+
                 r"cl.exe /LD /EHsc /Gz /Fecpparm /std:c++latest /DBUILDING_DLL /O2 armcpp.cpp"+
                 " -I "+ armadilloincludes +" /link " + armadillolibblas64)
         print(compile, file=sys.stderr)

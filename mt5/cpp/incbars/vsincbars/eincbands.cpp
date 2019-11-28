83dddfae90006570d70fb471647f59ba36439d52
Andre Ferreira <aflopes7@gmail.com>
2019-11-28 15:42:02 -0300

    class labelling increasing bands  - doing on paper first

--- mt5/cpp/incbars/vsincbars/eincbands.cpp
+++ mt5/cpp/incbars/vsincbars/eincbands.cpp
@@ -10,8 +10,8 @@
 // Using Dolar/Real Bars and consequently tick data
 // only for tick time-frame
 // Net Mode Only!
-// isInsideDay() should be checked by Metatrader prior doing 
-// any openning of positions   
+// isInsideDay() should be checked by Metatrader prior doing
+// any openning of positions
 
 // Base Data
 //std::shared_ptr<CCBufferMqlTicks> m_ticks; // buffer of ticks w. control to download unique ones.
@@ -37,14 +37,14 @@ double m_bbwindow_inc;   // are multiples of m_bbwindow_inc
 // feature indicators
 std::shared_ptr<CFracDiffIndicator> m_fd_mbarp; // frac diff on money bar prices
 
-// store buy|sell|hold signals for each bband
-std::vector<std::shared_ptr<CBandSignal>> m_raw_signal;
+// store buy|sell|hold signals for each bband - raw
+std::vector<std::shared_ptr<CBandSignal>> m_rbandsgs;
 
 std::vector<int> m_last_raw_signal; // last raw signal in all m_nbands
 int m_last_raw_signal_index; // related to m_bars buffer on refresh()
 
 // features and training vectors
-// array of buffers for each signal feature    
+// array of buffers for each signal feature
 int m_nsignal_features;
 int m_batch_size;
 int m_ntraining;
@@ -62,7 +62,7 @@ double m_run_targetprofit; //targetprofit in $$$ per order
 // max number of orders
 // position being a execution of one order or increment
 // on the same direction
-// number of maximum 'positions'                             
+// number of maximum 'positions'
 int m_max_positions; // maximum number of 'positions'
 int m_last_positions; // last number of 'positions'
 double m_last_volume; // last volume + (buy) or - (sell)
@@ -129,14 +129,16 @@ void Initialize(int nbands, int bbwindow,
     // only the space available for them
     //m_model_refresh = model_refresh;
 
-    m_raw_signal.resize(m_nbands); // one for each band
+    m_rbandsgs.resize(m_nbands); // one for each band
     m_last_raw_signal.resize(m_nbands);
 
-    // void CreateBBands() 
+    // void CreateBBands()
     // raw signal storage + ctalib bbands
     double inc = m_bbwindow_inc;
     for (int i = 0; i < m_nbands; i++) { // create multiple increasing bollinger bands
-        m_raw_signal[i].reset(new CBandSignal(m_bbwindow * inc, 2.5, 2, Expert_BufferSize)); // 2-Weighted type        
+        m_rbandsgs[i].reset(new CBandSignal(m_bbwindow * inc, 2.5, 2, Expert_BufferSize)); // 2-Weighted type
+        // weighted seams more meaning since i know each bar
+        // represents same ammount of money so have a equal weight
         inc += m_bbwindow_inc;
     }
 
@@ -176,7 +178,7 @@ int AddTicks(const MqlTick* cticks, int size) {
 }
 
 // Since all buffers must be alligned and also have same size
-// Expert_BufferSize 
+// Expert_BufferSize
 // Same new bars will come for all indicators and else
 // All are updated after bars so that's the main
 // begin of new bar is also begin of new data on all buffers
@@ -200,9 +202,9 @@ bool Refresh()
     // first is what you want to do and second what you want to try
     result = m_fd_mbarp->Refresh(m_bars->m_last.data(), 0, m_bars->m_nnew) && result;
 
-    // update signal based on all bollinger bands 
+    // update signals based on all bollinger bands
     for (int j = 0; j < m_nbands; j++) {
-        result = m_raw_signal[j]->Refresh(m_bars->m_last.data(), 0, m_bars->m_nnew) && result;
+        result = m_rbandsgs[j]->Refresh(m_bars->m_last.data(), 0, m_bars->m_nnew) && result;
     }
 
     return result;
@@ -250,7 +252,7 @@ void verifyEntry() {
         //        // modify the band raw signals by the ones predicted
         //        for (int i = 0; i < m_nbands; i++)
         //            // change from -1 to 1 or to 0
-        //            m_raw_signal[i].m_data[m_raw_signal[i].Count() - 1] = y_pred;
+        //            m_rbandsgs[i].m_data[m_rbandsgs[i].Count() - 1] = y_pred;
         //    }
         //}
     }
@@ -273,7 +275,7 @@ int lastRawSignals() {
     // on circular buffers index alligned on all
     for (int j = 0; j < m_nbands; j++) {
         for (int i = IdxNewData(); i < BufferTotal(); i++) {
-            m_last_raw_signal[j] = m_raw_signal[j]->At(i);
+            m_last_raw_signal[j] = m_rbandsgs[j]->At(i);
             if (m_last_raw_signal[j] != 0) {
                 if (direction == 0) {
                     direction = m_last_raw_signal[j];
@@ -296,6 +298,239 @@ int lastRawSignals() {
 }
 
 
+// Create target class by analysing raw bollinger band signals
+// those that went true receive 1 buy or -1 sell
+// those that wen bad receive 0 hold
+// A target class exist for each band.
+// An essemble of bands together and summed are a better classifier.
+// A day integer identifier must exist prior call.
+// That is used to blocking positions from passing to another day.
+void LabelClasses() {
+    for (int i = 0; i < m_nbands; i++) {
+        BandLabelClasses(*m_rbandsg[i], m_xypairs, i);
+    }
+}
+
+void BandLabelClasses(CCBuffer<double> bandsg,
+    CBuffer<XyPair> xypairs, int band_number)
+{
+    // targetprofit - profit to close open position
+    // amount - tick value * quantity bought in $$
+    double entryprice = 0;
+    int history = 0; // nothing, buy = 1, sell = -1
+    int day, previous_day = 0;
+    long time;
+    double profit = 0;
+    double quantity = 0; // number of contracts or stocks shares
+    // Starts from the past (begin of buffer of signals)
+    int i = 0;//
+    // OR
+    // For not adding again the same signal
+    // Starts where it stop the last time
+    if (xypairs.Count() > 0) {
+        int last_buff_index = m_times.QuickSearch(xypairs.GetData(0).time);
+        if (last_buff_index < 0) {
+            Print("This was not implemented and should never happen!");
+            return;
+        }
+        // next signal after this!
+        i = last_buff_index + 1;
+    }
+    for (; i < BufferTotal(); i++) { // from past to present
+        time = m_times[i].ms;
+        day = m_times[i].day; // unique day identifier
+        if(day != previous_day){ // a new day reset everything
+            if (history == 1 || history == -1) {
+                // the previous batch o/f signals will not be saved. I don't want to train with that
+                xypairs.RemoveLast();
+            }
+            entryprice = 0;
+            history = 0; //# 0= nothing, buy = 1, sell = -1
+            previous_day = day;
+        }
+        // first take care of what you have
+        // calculate profit / stoploss to see if should close
+        if(history != 0){
+            if(history == 1){ // it was a buy
+              profit = (m_bars[i].last - entryprice) * quantity;// # current profit
+              if(profit >= m_targetprofit){
+                  // xypairs.Last().y = 1 // a real (buy) index class nothing to do
+                  history = 0;
+              }
+              else
+              if (profit <= (-m_stoploss)){ // reclassify the previous buy as hold
+                  xypairs.Last().y = 0; // not a good deal, was better not to entry
+                  history = 0;
+              }
+           }
+           else{ // a sell -1
+             profit = (entryprice - m_bars[i].last) * quantity;// # current profit
+             if (profit >= m_targetprofit) {
+                 // xypairs.Last().y = -1 // a real (sell) index class nothing to do
+                 history = 0;
+             }
+             else
+             if (profit <= (-m_stoploss)) { // reclassify the previous sell as hold
+                 xypairs.Last().y = 0; // not a good deal, was better not to entry
+                 history = 0;
+             }
+          }
+       }
+       // get more positions or open a new one
+       else {
+         if (bandsg[i] == 1) {
+             switch (history) {
+               case 0:
+                   // new buy signal
+                   entryprice = m_bars[i].last; // last negotiated price
+                   xypairs.Add(new XyPair(1, m_times[i], band_number)); //  save this buy
+                   quantity = roundVolume(m_ordersize / entryprice);
+                   history = 1;
+               break;
+               case 1:
+                   // another buy in sequence
+                   // maybe it is dancing around this band boundary
+                   entryprice = m_bars[i].last;
+                   xypairs.Add(new XyPair(1, m_times[i], band_number)); //  save this buy
+                   quantity = roundVolume(m_ordersize / entryprice);
+                   history = 1;
+               break;
+               case -1;
+                   // a sell after a buy with no profit
+                   // the previous batch of signals will be saved with this class (hold)
+                   xypairs.Last().y = 0; // reclassify the previous buy/sell as hold
+               break;
+             }
+       }
+        // 4 main cases
+        // signal  1 - history = 0 || != 0  (total 1/0, 1/-1,, 1/1) = 3
+        // signal -1 - history = 0 || != 0  (total -1/0, -1/1,, -1/-1) = 3
+        // signal  0 - history = 1          (total 0/1) = 1
+        // signal  0 - history = -1         (total 0/-1) = 1
+        // useless : signal 0 - history = 0 - total total 3x3 = 9
+
+        else
+            if (bandsg[i] == -1) {
+                if (history == 0) {
+                    entryprice = m_bars[i].last;
+                    xypairs.Add(new XyPair(-1, m_times[i], band_number)); //  save this sell
+                }
+                else { // another sell in sequence
+                     // or after a buy  in the same minute
+                    // the previous batch of signals will be saved with this class (hold)
+                    xypairs.Last().y = 0; // reclassify the previous buy/sell as hold
+                    // new buy sell
+                    // maybe we can have profit here
+                    // in a fast movement
+                    entryprice = m_bars[i].last;
+                    xypairs.Add(new XyPair(-1, m_times[i], band_number)); //  save this buy
+                }
+                quantity = roundVolume(m_ordersize / entryprice);
+                history = -1;
+            }
+            else // signal 0 history 1
+                if (history == 1) { // equivalent to + && bandsg[i] == 0
+                    profit = (m_bars[i].last - entryprice) * quantity;// # current profit
+                    if (profit >= m_targetprofit) {
+                        // xypairs.Last().y = 0 // a real (buy) index class nothing to do
+                        history = 0;
+                    }
+                    else
+                        if (profit <= (-m_stoploss)) { // reclassify the previous buy as hold
+                            xypairs.Last().y = 0; // not a good deal, was better not to entry
+                            history = 0;
+                        }
+                }
+                else // signal 0 history -1
+                    if (history == -1) { // equivalent to + && bandsg[i] == 0
+                        profit = (entryprice - m_bars[i].last) * quantity;// # current profit
+                        if (profit >= m_targetprofit) {
+                            // xypairs.Last().y = 0 // a real (sell) index class nothing to do
+                            history = 0;
+                        }
+                        else
+                            if (profit <= (-m_stoploss)) { // reclassify the previous sell as hold
+                                xypairs.Last().y = 0; // not a good deal, was better not to entry
+                                history = 0;
+                            }
+                    }
+        // else signal 0 history 0
+    }
+    //# reached the end of data buffer did not close one buy previouly open
+    if (history == 1 || history == -1) // don't know about the future cannot train with this guy
+        xypairs.RemoveLast();
+}
+
+
+
+//void CreateXFeatureVectors(CObjectBuffer<XyPair>& xypairs)
+//{ // using the pre-filled buffer of y target classes
+//  // assembly the X array of features for it
+//  // from more recent to oldest
+//  // if it is already filled (ready) stop
+//    for (int i = 0; i < xypairs.Count(); i++) {
+//        // no need to assembly any other if this is already ready
+//        // olders will also be ready
+//        if (!CreateXFeatureVector(xypairs.GetData(i)))
+//            xypairs.RemoveData(i);
+//    }
+//
+//}
+//
+//bool CreateXFeatureVector(XyPair& xypair)
+//{
+//    if (xypair.isready) // no need to assembly
+//        return true;
+//
+//    // find xypair.time current position on all buffers (have same size)
+//    // time is allways sorted
+//    int bufi = m_times.QuickSearch(xypair.time);
+//
+//    // cannot assembly X feature train with such an old signal not in buffer anymore
+//    // such should be removed ...
+//    if (bufi < 0) { // not found -1 : this should never happen?
+//        Print("ERROR: This should never happen!");
+//        return false;
+//    }
+//    // not enough : bands raw signal, features in time to form a batch
+//    // cannot create a X feature vector and will never will
+//    // remove it
+//    if (bufi - m_batch_size < 0)
+//        return false;
+//
+//    if (ArraySize(xypair.X) < m_xtrain_dim)
+//        xypair.Resize(m_xtrain_dim);
+//
+//    int xifeature = 0;
+//    // something like
+//    // double[Expert_BufferSize][nsignal_features] would be awesome
+//    // easier to copy to X also to create cross-features
+//    // using a constant on the second dimension is possible
+//    //bufi = BufferTotal()-1-bufi;
+//    bufi -= m_batch_size;
+//    int batch_end_idx = bufi + m_batch_size;
+//    for (int timeidx = bufi; timeidx < batch_end_idx; timeidx++) {
+//        // from past to present
+//        // features from band signals
+//        for (int i = 0; i < m_nbands; i++, xifeature++) {
+//            xypair.X[xifeature] = m_rbandsgs[i][timeidx];
+//        }
+//        // fracdif feature
+//        xypair.X[xifeature++] = m_fd_mbarp[timeidx];
+//        // some indicators might contain EMPTY_VALUE == DBL_MAX values
+//        // verified volumes with EMPTY VALUES but I suppose that also might happen
+//        // with others in this case better drop the training sample?
+//        // sklearn throws an exception because of that
+//        // anyhow that's not a problem for the code since the python exception
+//        // is catched behind the scenes and a valid model will only be available
+//        // once correct samples are available
+//    }
+//    xypair.isready = true;
+//    return true; // suceeded
+//}
+//
+
+
 
 ////////////////////////////////////////////////
 ///////////////// Python API //////////////////
@@ -304,7 +539,7 @@ int lastRawSignals() {
 
 extern py::array_t<MoneyBar> *pymbars;
 
-// or by Python 
+// or by Python
 int pyAddTicks(py::array_t<MqlTick> ticks)
 {
     return AddTicks(ticks.data(), ticks.size());
@@ -312,7 +547,7 @@ int pyAddTicks(py::array_t<MqlTick> ticks)
 
 std::shared_ptr<py::array_t<MoneyBar>> ppymbars;
 
-py::array_t<MoneyBar> pyGetMoneyBars() {   
+py::array_t<MoneyBar> pyGetMoneyBars() {
     MoneyBar* pbuf = (MoneyBar*)ppymbars->request().ptr;
     for (size_t idx = 0; idx<Expert_BufferSize; idx++)
         pbuf[idx] = m_bars->m_data[idx];

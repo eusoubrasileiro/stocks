25dc966b219a1e8d37a3b4851e578907ced38819
Andre Ferreira <aflopes7@gmail.com>
2019-12-18 16:18:33 -0300

    Porting to C++ ticks fixing metatrader mess - print messages should go file

--- mt5/cpp/incbars/vsincbars/datastruct/ticks.cpp
+++ mt5/cpp/incbars/vsincbars/datastruct/ticks.cpp
@@ -26,39 +26,185 @@ void fixArrayTicks(std::vector<MqlTick> ticks){
 	}
 }
 
-//// circular buffer version
-//// 10k ticks maximum downloaded every time Refresh is called
-//// 1 ms time-frame suggested using OnTimer
-//CCBufferMqlTicks::CCBufferMqlTicks(int size) {
-//    m_nnew = 0;
-//    gticks = 0;
-//    CCBuffer::SetSize(size);
-//}
-//
-//int CCBufferMqlTicks::nNew() { return m_nnew; } // number of new ticks after calling Refresh()
-//
-//// just receive ticks from Python/Metatrader and add them
-//
-//int CCBufferMqlTicks::Refresh(std::vector<MqlTick>::iterator start, 
-//	std::vector<MqlTick>::iterator end){
-//	AddRange(start, end);
-//	m_nnew = std::distance(end, start);
-//	gticks += m_nnew;
-//	return m_nnew;
-//}
-//
-//int CCBufferMqlTicks::Refresh(MqlTick *carray, int csize) {
-//    AddRange(carray, csize);
-//    m_nnew = csize;
-//    gticks += m_nnew;
-//    return m_nnew;
-//}
-//
-//
-//
-//int CCBufferMqlTicks::indexesNewTicks(int& start1, int& end1,
-//		int& start2, int& end2) {
-//
-//		return indexesData(Count() - m_nnew, m_nnew,
-//			start1, end1, start2, end2);
-//}
+
+
+// circular buffer version
+// 10k ticks maximum downloaded every time Refresh is called
+// 1 ms time-frame suggested using OnTimer
+bool MoneyBarBuffer::correctMt5UnrealTicks(){
+  int real_nnew = 0;
+  int begin_refpos = m_refpos;
+  for(int i=0; i<m_nnew && m_refpos < m_refsize; i++){
+    if(m_copied_ticks[i].time_msc
+        == m_refticks[m_refpos].time_msc){
+      real_nnew++; m_refpos++;
+    }
+  }
+  // testing needs boundaries of data chuncks
+  m_bound_ticks[ib_tick++] = m_refpos;
+  ///////
+  AddRange(m_refticks, begin_refpos, m_refpos);
+  m_nnew = real_nnew;
+  if(m_nnew==0){ // number of trash ticks created by Metatrader
+    m_trash_count++;
+    return false;
+  }
+  if(m_trash_count > 0){
+		// can be a in a log file certainly!!! if verbose set
+    Print("Ignored metatrader 5 created unreal ticks : ",
+    m_trash_count, " at ",
+    (datetime) m_cmpbegin_time/1000);
+    m_trash_count = 0;
+  }
+  return true;
+}
+
+  void loadCorrectTicks(string symbol){
+    isbacktest = false;
+    if(MQLInfoInteger(MQL_TESTER)){ // must load specific file with ticks
+      // located on COMMON folder
+      // if ticks on this file is not compatible with
+      // your tick history problems will come
+      // advised to use a custom symbol to avoid that
+      isbacktest = true;
+      StringAdd(m_refcticks_file, symbol);
+      StringAdd(m_refcticks_file,"_mqltick.bin");
+      m_refsize = ReadTicks(m_refticks, m_refcticks_file, FILE_COMMON);
+      // find begin for current backtest
+      long timebegin_ms = (long) TimeCurrent()*1000;
+      for(m_refpos = 0; m_refpos<m_refsize; m_refpos++)
+          if(m_refticks[m_refpos].time_msc >= timebegin_ms) break;
+      m_trash_count = 0;
+
+      ArrayResize(m_bound_ticks, m_refsize); // certainly will be smaller
+      ib_tick = 0;
+    }
+  }
+
+//////////////////////////////////////////////
+//////////////////////////////////////////////
+
+  bool beginNewTicks(){
+    scheck = false;
+    // ticks are indexed from the past to the present,
+    ArraySetAsSeries(m_copied_ticks, false); // cancel this
+    // find index of first tick different
+    // from what we already have using the comparision indexes
+    if(isfull == 0 && m_cposition == 0){ // we have nothing - copy everything
+      m_bgidx = 0; scheck = true;
+    }
+    else{
+      for(int i=0; i<m_ncopied; i++)
+        if(m_copied_ticks[i].time_msc == At(m_cmpbegin+i).time_msc){
+          scheck=true; // must have at least one sample equal
+          // for security check of sync with server
+        }
+        else{ // found begin new data
+          m_bgidx = i;
+          break;
+        }
+    }
+    // the difference is the number of new ticks
+    m_nnew = m_ncopied - m_bgidx;
+    return (scheck);
+  }
+
+public:
+
+  CCBufferMqlTicks(void);
+
+  CCBufferMqlTicks(string symbol){
+      m_symbol = symbol;
+      ArrayResize(m_copied_ticks, Max_Tick_Copy);
+      m_ncopied = 0;
+      m_nnew = 0;
+      // time now in ms will work 'coz next tick.ms value will be bigger
+      // and will take its place
+      m_cmpbegin_time = long (TimeCurrent())*1000; // turn in ms
+      m_cmpbegin = 0;
+
+      loadCorrectTicks(symbol);
+  }
+
+  ~CCBufferMqlTicks(){
+    ArrayFree(m_copied_ticks);
+  }
+
+  int nNew(){ return m_nnew; } // number of new ticks after calling Refresh()
+
+  int Refresh(){
+   // copy all ticks from last copy time - 1 milisecond to now
+   // to avoid missing ticks on same ms)
+   m_ncopied = CopyTicksRange(m_symbol, m_copied_ticks,
+        COPY_TICKS_ALL, m_cmpbegin_time, 0);
+
+   if(m_ncopied < 1){
+      int error = GetLastError();
+      if(error == ERR_HISTORY_TIMEOUT)
+        // ticks synchronization waiting time is up, the function has sent all it had.
+        Print("ERR_HISTORY_TIMEOUT");
+      else
+      if(error == ERR_HISTORY_SMALL_BUFFER)
+        //static buffer is too small. Only the amount the array can store has been sent.
+        Print("ERR_HISTORY_SMALL_BUFFER");
+      else
+      if(error == ERR_NOT_ENOUGH_MEMORY)
+        // insufficient memory for receiving a history from the specified range to the dynamic
+        // tick array. Failed to allocate enough memory for the tick array.
+        Print("ERR_NOT_ENOUGH_MEMORY");
+      // better threatment here ??...better ignore and wait for next call
+      return -1;
+   }
+
+   if(!beginNewTicks()){
+     Print("Data Without Sync With Server!");
+     Print("Forcing Stop");
+     int error = 0; error = 1/error;  // division by 0 stops EA
+   }
+
+    // allign array of new ticks to zero
+    ArrayCopy(m_copied_ticks, m_copied_ticks, 0, m_bgidx, m_nnew);
+    // fix nonsense last, ask, bid empty
+    fixArrayTicks(m_copied_ticks);
+
+    // if on back-test
+    // dont use ticks from metatrader, only times!
+    if(isbacktest){
+      if(!correctMt5UnrealTicks())
+        return 0; // no new ticks
+    }
+    else{ // real time operation
+      AddRange(m_copied_ticks, 0, m_nnew);
+    }
+
+    // get begin time of comparison to check for new ticks
+    // last tick ms -1 on buffer
+    // get index of first tick with time >= (last tick ms) - (1 ms)
+    m_cmpbegin_time = m_copied_ticks[m_nnew-1].time_msc-1;
+    m_cmpbegin = 0;
+    for(int i=Count()-1; i>=0; i--)
+      if(At(i).time_msc < m_cmpbegin_time){
+        m_cmpbegin = i+1; break;
+      }
+
+    gticks += m_nnew;
+
+    return m_nnew;
+  }
+
+};
+
+
+int ReadTicks(MqlTick &mqlticks[], string filename, int file_acces=0){
+
+    ArrayResize(mqlticks, 100e6); // just to guarantee 100MM
+    int handle = FileOpen(filename, file_acces|FILE_READ|FILE_BIN);
+    if(handle==INVALID_HANDLE)
+        return -1;
+    int size = FileReadArray(handle, mqlticks);
+    FileClose(handle);
+    ArrayResize(mqlticks, size);
+
+
+    return size;
+}
--- mt5/cpp/incbars/vsincbars/include/ticks.h
+++ mt5/cpp/incbars/vsincbars/include/ticks.h
@@ -21,39 +21,64 @@ struct MqlTick
 }; // sizeof is 60 bytes to avoid auto alignment to 64 by compiler #pragma pack(2) above is needed
 #pragma pack(pop)
 
-// 2      TICK_FLAG_BID –  tick has changed a Bid price
-// 4      TICK_FLAG_ASK  – a tick has changed an Ask price
-// 8      TICK_FLAG_LAST – a tick has changed the last deal price
-// 16     TICK_FLAG_VOLUME – a tick has changed a volume
-// 32     TICK_FLAG_BUY – a tick is a result of a buy deal
-// 64     TICK_FLAG_SELL – a tick is a result of a sell deal
+// 2      TICK_FLAG_BID ï¿½  tick has changed a Bid price
+// 4      TICK_FLAG_ASK  ï¿½ a tick has changed an Ask price
+// 8      TICK_FLAG_LAST ï¿½ a tick has changed the last deal price
+// 16     TICK_FLAG_VOLUME ï¿½ a tick has changed a volume
+// 32     TICK_FLAG_BUY ï¿½ a tick is a result of a buy deal
+// 64     TICK_FLAG_SELL ï¿½ a tick is a result of a sell deal
 
 
-// Since Ticks will only come to C++ 
-// cleanned there is no need for this class
-// since the C++ code works with moneybars
+// Messed ticks from Meta5 will also come to C++
+// here we fix then replacing by a file of correct ones
 
-//const int Max_Tick_Copy = 10e3;
-//
-//// circular buffer version
-//// 10k ticks maximum downloaded every time Refresh is called
-//// 1 ms time-frame suggested using OnTimer
-//class CCBufferMqlTicks : public CCBuffer<MqlTick>
-//{
-//protected:
-//	int m_nnew;
-//public:
-//	int gticks = 0; // global counter of ticks
-//
-//	CCBufferMqlTicks(int size);
-//	int nNew(); // number of new ticks after calling Refresh()
-//
-//    // just receive ticks from Python/Metatrader and add them
-//	int Refresh(std::vector<MqlTick>::iterator start, std::vector<MqlTick>::iterator end);
-//    int Refresh(MqlTick *carray, int csize);
-//
-//	int indexesNewTicks(int& start1, int& end1,
-//		int& start2, int& end2);
-//};
+const int Max_Tick_Copy = 10e3;
 
+// circular buffer version
+// 10k ticks maximum downloaded every time Refresh is called
+// 1 second time-frame suggested using OnTimer
+class BufferMqlTicks : public buffer<MqlTick>
+{
+protected:
+  string m_symbol;
+  MqlTick m_copied_ticks[]; // fixed size number of ticks copied every x seconds
+  int m_ncopied; // last count of ticks copied on buffer
+  int m_nnew; // number of new ticks last copied
+  bool scheck; // security check of sync with data server
+  int64_t m_cmpbegin_time;
+  int m_cmpbegin;
+  int m_bgidx; // temp:: where the new ticks starts on array m_copied_ticks
 
+  //////////////////////////////////////////////
+  ////// backtest workaround for stupid mt5 fake ticks creation
+  bool isbacktest;
+  MqlTick m_refticks[]; // correct ticks without stupid modifications
+  int m_refsize; // size of reference data
+  // on volume etc by MT5 to fuc** with everything serious
+  std::string m_refcticks_file;
+  int m_refpos; // position on reference ticks file
+  int m_trash_count;
+  ////////////////////////////////////////////
+  ////// for testing against Python/C++ since i need to
+  ////// know boundaries of new ticks (since data comes in chuncks)
+  int m_bound_ticks[];
+  int ib_tick; // count of calls to AddRange
+
+  bool correctMt5UnrealTicks();
+
+  void loadCorrectTicks(string symbol);
+//////////////////////////////////////////////
+//////////////////////////////////////////////
+  bool beginNewTicks();
+
+public:
+
+  BufferMqlTicks(void);
+
+  BufferMqlTicks(std::string symbol);
+
+  int nNew(){ return m_nnew; } // number of new ticks after calling Refresh()
+
+  int Refresh(MqlTick *ticks, int count); // will be called somehow by mt5
+
+};

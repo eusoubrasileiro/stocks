cbb10d5c4343f621c1dd5a0c453b0a6faf666679
Andre Ferreira <aflopes7@gmail.com>
2019-12-18 09:01:02 -0300

    dtp missing initialization value

--- mt5/cpp/incbars/vsincbars/datastruct/bars.cpp
+++ mt5/cpp/incbars/vsincbars/datastruct/bars.cpp
@@ -12,6 +12,9 @@ MoneyBarBuffer::MoneyBarBuffer() {
     m_pvs = m_vs = 0; // v. weighted price for this bar
     m_moneybarsize = DBL_EMPTY_VALUE;
     m_point_value = 0;
+    m_bar.bidh = m_bar.bidl = tick.bid = 0; // not sure this is needed
+    m_bar.askh = m_bar.askl = tick.ask = 0;
+    m_bar.smsc - m_bar.emsc = 0;  // first dtp calc. needs this
 }
 
 // return only new ... data added
@@ -29,7 +32,7 @@ void MoneyBarBuffer::Init(double tickvalue, double ticksize, double moneybarsize
 // return number created
 size_t MoneyBarBuffer::AddTick(MqlTick tick) {
     m_nnew = 0;
-    // get high and low bid-ask 
+    // get high and low bid-ask
     if (tick.ask > m_bar.askh)
         m_bar.askh = tick.ask;
     else
@@ -41,7 +44,7 @@ size_t MoneyBarBuffer::AddTick(MqlTick tick) {
         if (tick.bid < m_bar.bidl)
             m_bar.bidl = tick.bid;
     ///////////////////////////
-    if (tick.volume > 0) { // there was a deal        
+    if (tick.volume > 0) { // there was a deal
         // control to not have bars with ticks of different days
         gmtime_s(&ctime, &tick.time);
         if (m_bar.nticks == 0) {// entry time for this bar
@@ -50,23 +53,23 @@ size_t MoneyBarBuffer::AddTick(MqlTick tick) {
             // time difference in seconds to previous bar
             m_bar.dtp = 0.001 * (m_bar.smsc - m_bar.emsc);
             m_bar.bidh = m_bar.bidl = tick.bid;
-            m_bar.askh = m_bar.askl = tick.ask;            
+            m_bar.askh = m_bar.askl = tick.ask;
         }
         else // just crossed to a new day
-        if(ctime.tm_wday != m_bar.time.tm_wday){ 
-            // clean up (ignore) previous data 
+        if(ctime.tm_wday != m_bar.time.tm_wday){
+            // clean up (ignore) previous data
             // for starting a new bar
             m_bar.nticks = 0;
             m_bar.time = ctime;
             m_bar.smsc = tick.time_msc;
-            m_bar.dtp = 0; // no previous bar 
+            m_bar.dtp = 0; // no previous bar
             m_count_money = 0;
             m_pvs = m_vs = 0;
             m_bar.bidh = m_bar.bidl = tick.bid;
             m_bar.askh = m_bar.askl = tick.ask;
-        }       
+        }
         m_bar.nticks++;
-        m_count_money += tick.volume_real * tick.last * m_point_value;        
+        m_count_money += tick.volume_real * tick.last * m_point_value;
         while (m_count_money >= m_moneybarsize) { // may need to create many bars for one tick
             // in this case nticks == 0 for the second one and so forth
             // start/exit time also == 0
@@ -78,11 +81,11 @@ size_t MoneyBarBuffer::AddTick(MqlTick tick) {
             uidtimes.add(m_bar.uid);
             add(m_bar);
             m_pvs = m_vs = 0;
-            m_bar.nticks = 0; 
+            m_bar.nticks = 0;
             m_bar.dtp = 0;
             // case when multiple bars created from same tick
             // start and exit time are the same
-            m_bar.smsc = tick.time_msc;            
+            m_bar.smsc = tick.time_msc;
             // wil be overwritten when a new tick
             // createas a new bar
             m_count_money -= m_moneybarsize;
@@ -127,7 +130,7 @@ inline bool cmpMoneyBarSmallUid(const MoneyBar& a, uint64_t uid) {
 //inline bool CmpMoneyBarGreaterUid(const MoneyBar& a, const unsigned long long uid) {
 //    return a.uid > uid;
 //}
-// then you can use 
+// then you can use
 // return index on ring buffer or -1 on not finding
 size_t MoneyBarBuffer::Search(uint64_t uid)
 {
@@ -136,20 +139,20 @@ size_t MoneyBarBuffer::Search(uint64_t uid)
 }
 
 // std::lower_bound
-// Returns an iterator pointing to the first element in the range [first,last) 
+// Returns an iterator pointing to the first element in the range [first,last)
 // which does not compare less than val.
 // The elements are compared using operator< or comparator function (cmpMoneyBarSmallUid)
-// since uid 
+// since uid
 // 1. does not repeat
 // 2. are sorted
 // above will get the index of the uid on the buffer of bars
 
-// find first money bar with start time bigger or equal than 
+// find first money bar with start time bigger or equal than
 inline bool cmpMoneyBarStime(const MoneyBar& a, int64_t smsc) {
     return a.smsc < smsc;
 }
 
-// return index on ring buffer for 
+// return index on ring buffer for
 // for first money bar with start time bigger or equal than or -1 not finding
 size_t MoneyBarBuffer::SearchStime(int64_t smsc)
 {

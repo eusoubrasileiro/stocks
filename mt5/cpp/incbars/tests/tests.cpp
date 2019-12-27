6413be43eef33e26b1fb1aa1d7ab2edc4ef738dd
André <eusoubrasileiro@gmail.com>
2019-12-26 22:27:09 -0300

    Need clean-up python code. - model can be a global variable, no need to send/get model python/c++

--- mt5/cpp/incbars/tests/tests.cpp
+++ mt5/cpp/incbars/tests/tests.cpp
@@ -350,15 +350,17 @@ import numpy as np
 from sklearn.ensemble import ExtraTreesClassifier
 import pickle
 
-def pyTrainModel(X, y) :
-    trees = ExtraTreesClassifier(n_estimators = 120, verbose = 0)
+trees = ExtraTreesClassifier(n_estimators = 120, verbose = 0)
+
+def pyTrainModel(X, y):
+    global trees
     trees.fit(X, y)
     # save model
     str_trees = pickle.dumps(trees)
     return str_trees # easier to pass to C++
 
-def pyPredictwModel(X, str_trees) :
-    trees = pickle.loads(str_trees);
+def pyPredictwModel(X, str_trees):
+    global trees
     return trees.predict(X)[0]
 )";
 

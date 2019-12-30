5ce597d70675fb08e2ab445d42ea340555a70830
Andre Ferreira <aflopes7@gmail.com>
2019-12-30 16:14:28 -0300

    Marcos Lopez de Prado PurgedKFold. - removing overlaping information from training samples (leaking prevention)

--- algos/cv.py
+++ algos/cv.py
@@ -1,5 +1,6 @@
 """tools for cross-validate a classification model sklearn mainly"""
 import numpy as np
+import pandas as pd
 
 def indexSequentialFolds(length, size, verbose=True):
     """
@@ -186,3 +187,93 @@ class sKFold(object):
 #         for start, end in self.split_indexes:
 #             sval = start+ntrain
 #             yield list(range(start, sval)), list(range(sval, end))
+
+
+
+# Marcos Lopez de Prado
+# PurgedKFold
+# removing information from training samples (leaking prevention)
+
+
+#`lbsigns['twhen']` and `lbsigns['tdone']`
+# are start and end index of bars needed to form that specific Xy pair
+# works like this t1 = pd.Series(lbsigns.tdone, index=lbsigns.twhen)
+def getTrainTimes(t1,testTimes):
+    """
+    Given testTimes, find the times of the training observations.
+    —t1.index: Time when the observation started.
+    —t1.value: Time when the observation ended.
+    —testTimes: Times of testing observations.
+    """
+    trn=t1.copy(deep=True)
+    for i,j in testTimes.iteritems():
+        df0=trn[(i<=trn.index)&(trn.index<=j)].index # train starts within test
+        df1=trn[(i<=trn)&(trn<=j)].index # train ends within test
+        df2=trn[(trn.index<=i)&(j<=trn)].index # train envelops test
+        trn=trn.drop(df0.union(df1).union(df2))
+    return trn
+
+from sklearn.model_selection._split import _BaseKFold
+
+
+# works like this
+# dfX = pd.DataFrame(X, index=t1.index)
+# dfy = pd.DataFrame(y, index=t1.index)
+# pkfold = cv.PurgedKFold(t1=t1)
+# for idxt, idxs in pkfold.split(dfX):
+#     print(len(idxt), len(idxs))
+class PurgedKFold(_BaseKFold):
+    """
+    Extend KFold class to work with labels that span intervals
+    The train is purged of observations overlapping test-label intervals
+    Test set is assumed contiguous (shuffle=False), w/o training samples in between
+    """
+    def __init__(self,n_splits=3,t1=None,pctEmbargo=0.):
+        if not isinstance(t1,pd.Series):
+            raise ValueError('Label Through Dates must be a pd.Series')
+        super(PurgedKFold,self).__init__(n_splits,shufﬂe=False,random_state=None)
+        self.t1=t1
+        self.pctEmbargo=pctEmbargo
+
+    def split(self,X,y=None,groups=None):
+        if (X.index==self.t1.index).sum()!=len(self.t1):
+            raise ValueError('X and ThruDateValues must have the same index')
+        indices=np.arange(X.shape[0])
+        mbrg=int(X.shape[0]*self.pctEmbargo)
+        test_starts=[(i[0],i[-1]+1) for i in \
+        np.array_split(np.arange(X.shape[0]),self.n_splits)]
+        for i,j in test_starts:
+            t0=self.t1.index[i] # start of test set
+            test_indices=indices[i:j]
+            maxT1Idx=self.t1.index.searchsorted(self.t1[test_indices].max())
+            train_indices=self.t1.index.searchsorted(self.t1[self.t1<=t0].index)
+            if maxT1Idx<X.shape[0]: # right train (with embargo)
+                train_indices=np.concatenate((train_indices,indices[maxT1Idx+mbrg:]))
+            yield train_indices,test_indices
+
+from sklearn.metrics import log_loss, accuracy_score, confusion_matrix
+
+# I dont care I cant see any usage for sample_weight
+def cvScore(clf,X,y,sample_weight=None,scoring='neg_log_loss',t1=None,cv=None,cvGen=None,
+pctEmbargo=None):
+    if scoring not in ['neg_log_loss','accuracy']:
+        raise Exception('wrong scoring method.')
+    if cvGen is None:
+        cvGen=PurgedKFold(n_splits=cv,t1=t1,pctEmbargo=pctEmbargo) # purged
+    score=[]
+    for train,test in cvGen.split(X=X):
+        ﬁt=clf.ﬁt(X=X.iloc[train,:],y=y.iloc[train].values.ravel())
+        #,sample_weight=sample_weight.iloc[train].values)
+        prob=ﬁt.predict_proba(X.iloc[test,:])
+        score_=-log_loss(y.iloc[test].values.ravel(),prob,
+        labels=clf.classes_)
+        #sample_weight=sample_weight.iloc[test].values,labels=clf.classes_)
+        pred=ﬁt.predict(X.iloc[test,:])
+        cm = confusion_matrix(y.iloc[test].values.ravel(), pred)
+        if cm.shape[0] < 2: # case only 1 class on both vectors
+            acc =  1 if (np.unique(y.iloc[test].values.ravel()) == np.unique(pred)) else 0
+        else:
+            acc = cm[0, 0]/(cm[0, 0]+cm[1, 0]) # for class 0 what matters
+        score_ = [score_, acc, len(X.iloc[train,:]), len(X.iloc[test,:])]
+        score.append(score_)
+    return np.array(score)

003961ad5dd1901e869d65671ea6a0a4084d7199
André <eusoubrasileiro@gmail.com>
2020-04-09 14:56:48 -0300

    Fixing Anaconda3 Python Path

--- config/meta5_py37.bat
+++ config/meta5_py37.bat
@@ -1,6 +1,7 @@
 set PYTHONHOME=%USERPROFILE%\Anaconda3
 set PYTHONPATH=%PYTHONHOME%\DLLs;%PYTHONHOME%\Lib;%PYTHONHOME%\Lib\site-packages;
 set Path=%Path%;%PYTHONHOME%;%PYTHONHOME%\DLLs;%PYTHONHOME%\Lib;%PYTHONHOME%\Lib\site-packages;%PYTHONHOME%\Scripts;%PYTHONHOME%;
+set Path=%Path%;%PYTHONHOME%\Library\bin;%PYTHONHOME%\Library\usr\bin;%PYTHONHOME%\mingw-w64\bin;
 D:
 cd "D:\MetaTrader 5"
 start /b terminal64.exe
--- config/setpy37.bat
+++ config/setpy37.bat
@@ -1,3 +1,4 @@
-set PYTHONHOME=%USERPROFILE%\AppData\Local\Programs\Python\Python37
+set PYTHONHOME=%USERPROFILE%\Anaconda3
 set PYTHONPATH=%PYTHONHOME%\DLLs;%PYTHONHOME%\Lib;%PYTHONHOME%\Lib\site-packages;
 set Path=%Path%;%PYTHONHOME%;%PYTHONHOME%\DLLs;%PYTHONHOME%\Lib;%PYTHONHOME%\Lib\site-packages;%PYTHONHOME%\Scripts;%PYTHONHOME%;
+set Path=%Path%;%PYTHONHOME%\Library\bin;%PYTHONHOME%\Library\usr\bin;%PYTHONHOME%\mingw-w64\bin;

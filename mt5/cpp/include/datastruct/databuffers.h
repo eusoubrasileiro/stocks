487b36d4760ec4924d9a2fc6d1b4bd138cecbef6
André <eusoubrasileiro@gmail.com>
2020-07-10 15:29:28 -0300

    splitting data from mt5 indicators

--- mt5/cpp/include/datastruct/databuffers.h
+++ mt5/cpp/include/datastruct/databuffers.h
@@ -16,34 +16,16 @@
 #include <string>
 #include <iostream>
 #include <time.h>
-
-DLL_EXPORT void CppDataBuffersInit(double ticksize, double tickvalue,
-    double moneybar_size,  // R$ to form 1 money bar
-    char* cs_symbol,  // cs_symbol is a char[] null terminated string (0) value at end
-    // SADF part
-    bool sadfindicator, // wether to load sadf indicator or not in bakground
-    int maxwindow,
-    int minwindow,
-    int order,
-    bool usedrift,
-    int maxbars,
-    int numcolors);
-
-DLL_EXPORT void CppGetSADFWindows(int* minwin, int* maxwin);
+#include "cwindicators.h"
 
 DLL_EXPORT int64_t CppOnTicks(MqlTick* mt5_pticks, int mt5_nticks, double *lost_ticks);
-
-DLL_EXPORT int CppMoneyBarMt5Indicator(double* mt5_ptO, double* mt5_ptH, double* mt5_ptL, double* mt5_ptC, double* mt5_ptM, unixtime* mt5_petimes, double* mt5_bearbull, int mt5ptsize);
-
-DLL_EXPORT int CppSADFMoneyBars(double* mt5_SADFline, double* mt5_SADFdots, double* mt5_imaxadfcolor, double* mt5_imaxadflast, int mt5ptsize);
-
-
-DLL_EXPORT BufferMqlTicks* GetBufferMqlTicks();
 
 DLL_EXPORT void CppTicksToFile(char* cs_filename);
 
 DLL_EXPORT bool CppisInsideFile(char* cs_filename);
 
-DLL_EXPORT bool CppNewBars(); // are there any new bars after call of CppOnTicks
-
-DLL_EXPORT size_t SearchMoneyBars(buffer<MoneyBar> mbars, uint64_t uid);
+DLL_EXPORT void CppDataBuffersInit(
+    double ticksize,
+    double tickvalue,
+    double moneybar_size,  // R$ to form 1 money bar
+    char*  cs_symbol);     // cs_symbol is a char[] null terminated string (0) value at end
